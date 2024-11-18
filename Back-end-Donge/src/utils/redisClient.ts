1f05424747d74f0e2f35b933870a612e4daad3c8
Mhtrobot <mhtrobot760@gmail.com>
2024-11-18 13:59:09 +0330

    init redis and logger

---
+++ Back-end-Donge/src/utils/redisClient.ts
@@ -0,0 +1,23 @@
+import {singleton} from "tsyringe";
+import { createClient } from "redis"
+import logger from "../core/logger";
+
+@singleton()
+export class RedisClient {
+    private client = createClient({url: process.env.REDIS_URL || 'redis://localhost:6379'});
+
+    constructor() {
+        this.client.on('error', (err) => {
+            logger.error(`Redis Client error: ${err}`);
+        });
+        this.client.connect();
+    }
+
+    async setEx(key: string, duration: number, value: string) {
+        await this.client.setEx(key, duration, value);
+    }
+
+    async get(key: string) {
+        return await this.client.get(key);
+    }
+}
