21d437debfbc78461fdb507f8ff890f3e322a9a5
Mhtrobot <mhtrobot760@gmail.com>
2024-11-28 17:22:43 +0330

    change dir

--- Back-end-Donge/IAMservice/src/index.ts
+++ Back-end-Donge/IAMservice/src/index.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import './container';
 import dotenv from "dotenv";
 dotenv.config();
-import app from  "./server";
+import app from "./server";
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
