576524115b6bdb53a4101f90c38da9eca223958a
Mohammad Hosseinvand Tahmasbi <53100398+Mhtrobot@users.noreply.github.com>
2024-12-19 19:07:21 +0000

    init core services

---
+++ Back-end-Donge/CoreService/src/index.ts
@@ -0,0 +1,10 @@
+// import 'reflect-metadata';
+// import './container';
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./server";
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
---
+++ Back-end-Donge/CoreService/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import fs from "fs";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'swagger-output.json'), 'utf-8'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+export default app;
